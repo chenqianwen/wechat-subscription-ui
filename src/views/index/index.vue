02fd722282fddf3a5713a49016d856f1d18dbbb0
chenqianwen <1433414364@qq.com>
2018-08-05 14:33:03 +0800

    init

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,5 +1,10 @@
 <template>
-    <h1>首页<Button type="primary" @click.prevent="breakEgg"></Button></h1>
+    <div>
+        <h1>首页
+            <Button type="primary" @click.prevent="breakEgg"></Button>
+        </h1>
+        <router-link to="/egg">去抽奖</router-link>
+    </div>
 </template>
 
 <script>
